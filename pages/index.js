1dbec29b079008fb0a306b5226bfb43dc0def83b
Alex Smith <fnkcollective@hotmail.co.uk>
2021-01-16 12:28:59 +0000

    Adds title to LayoutHeader

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Layout } from "../lib/components/Layout/Layout";
 export default function Home() {
   return (
     <Layout>
-      <Layout.Header>Header</Layout.Header>
+      <Layout.Header title="May, 2020" />
       <Layout.Main>Main Stuff</Layout.Main>
     </Layout>
   );
