d8e86b3af0df6b740c70aca04bb422bb3939e7e6
Alex Smith <fnkcollective@hotmail.co.uk>
2021-01-20 17:55:54 +0000

    Adds calendars API functions, hooks and docs

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,39 +1,20 @@
-import { useEffect, useContext, useState } from "react";
+import { useEffect } from "react";
 import { Layout } from "../lib/components/Layout/Layout";
 import { useRouter } from "next/router";
 import { useAuth } from "../lib/hooks/auth/useAuth";
-import { CalendarAPIContext } from "../lib/providers/CalendarAPIProvider";
-import { listCalendars } from "../lib/api/calendars/listCalendars";
+import { useFetchCalendars } from "../lib/hooks/api/useFetchCalendars";
 
 export default function Calendar() {
-  const { loading: loadingCalendarAPI } = useContext(CalendarAPIContext);
-  const { signOut, signedIn, loading: loadingAuth } = useAuth();
   const router = useRouter();
-  const [error, setError] = useState(null);
+
+  const { signOut, signedIn, loading: loadingAuth } = useAuth();
+  const { error } = useFetchCalendars();
 
   useEffect(() => {
     if (!loadingAuth && !signedIn) {
       router.push("/");
     }
   }, [signedIn]);
-
-  const fetchCalendars = async () => {
-    const { calendarsList, error } = await listCalendars();
-
-    if (error) {
-      return setError(error);
-    }
-
-    console.log(calendarsList);
-  };
-
-  useEffect(() => {
-    if (loadingCalendarAPI) {
-      return;
-    }
-
-    fetchCalendars();
-  }, [loadingCalendarAPI]);
 
   if (error) {
     return <p>Something went wrong 😭</p>;
