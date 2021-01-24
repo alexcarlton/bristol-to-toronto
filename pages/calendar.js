c97ab2cd4296e3832866b6a980a947e79560ad93
Alex Smith <altaudiolabs@gmail.com>
2021-01-24 09:49:49 +0000

    Adds dynamic header date time

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,15 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Layout } from "../lib/components/Layout/Layout";
 import { useRouter } from "next/router";
 import { useAuth } from "../lib/hooks/auth/useAuth";
 import { useFetchCalendars } from "../lib/hooks/api/useFetchCalendars";
 import { Sidebar } from "../lib/components/Sidebar/Sidebar";
 import { Calendar } from "../lib/components/Calendar/Calendar";
+import { DateTime } from "luxon";
 
 export default function CalendarPage() {
   const router = useRouter();
   const { signedIn, loading: loadingAuth } = useAuth();
   const { error } = useFetchCalendars();
+
+  const [dateTime] = useState(DateTime.local());
 
   useEffect(() => {
     if (!loadingAuth && !signedIn) {
@@ -21,9 +24,13 @@ export default function CalendarPage() {
     return <p>Something went wrong 😭</p>;
   }
 
+  const headerTitle = `${dateTime.toLocaleString({
+    month: "long",
+  })}, ${dateTime.toLocaleString({ year: "numeric" })}`;
+
   return (
     <Layout>
-      <Layout.Header title="May, 2020" />
+      <Layout.Header title={headerTitle} />
       <Layout.Main>
         <Sidebar />
         <Calendar />
